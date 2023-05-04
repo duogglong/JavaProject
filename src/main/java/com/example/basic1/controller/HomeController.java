9f1a36c4a3f726e3fcf612e66ea943fd08ab8bed
duogglong <long22262@gmail.com>
2023-05-04 22:13:43 +0700

    s

--- src/main/java/com/example/basic1/controller/HomeController.java
+++ src/main/java/com/example/basic1/controller/HomeController.java
@@ -1,23 +1,24 @@
 package com.example.basic1.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Controller
 @RequestMapping(value = "/home")
+@Slf4j
 public class HomeController {
     @GetMapping("/1")
     public String home1(Model model) {
+        log.info("Call api HomeController.home1");
         return "home";
     }
 
     @GetMapping("/2")
     public String home2(Model model) {
+        log.info("Call api HomeController.home2");
         return "home2";
     }
 }
