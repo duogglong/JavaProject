a1a38c2ca12172b93946754e57ff98960186f874
long22262@gmail.com <long22262@gmail.com>
2023-05-09 22:10:02 +0700

    change to 8026

--- src/main/java/com/example/basic1/controller/HomeController.java
+++ src/main/java/com/example/basic1/controller/HomeController.java
@@ -1,18 +1,18 @@
 package com.example.basic1.controller;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Date;
+
 @Controller
 @RequestMapping(value = "/home")
-@Slf4j
 public class HomeController {
     @GetMapping
     public String home(Model model) {
-        log.info("Call api HomeController.home");
+        System.out.println(new Date() + ": Call home()");
         return "index";
     }
 }
